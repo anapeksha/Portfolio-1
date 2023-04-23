79887d943734f947d14a17b359668cad105941c9
soharoy <124602360+soharoy@users.noreply.github.com>
2023-04-23 21:50:35 +0530

    added description in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,23 @@ const Home = () => {
 				Your go-to freelance content writer and editor!
 			</Typography>
 			<Grid container spacing={4}>
+				<Grid xs={12} item>
+					<Card>
+						<CardContent>
+							<Typography>
+								As a Content Specialist with over 3 years of experience, I am
+								passionate about crafting engaging and impactful content that
+								resonates with audiences. With expertise in diverse niches
+								including Finance, Education, Sports, iGames, News, Travel, IT,
+								and more, I strive to create content that informs, inspires, and
+								captivates readers. Name your expectations and you will have it!
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid xs={12} item>
+					<Typography variant="h5">Why choose me?</Typography>
+				</Grid>
 				{whyChooseMe.map((item, index) => {
 					return (
 						<Grid xs={12} item key={index}>
