ad1997138fb7703522f8bb77b07b2f9ee9f002b4
soharoy <124602360+soharoy@users.noreply.github.com>
2023-05-02 09:31:46 +0530

    loading skeleton added

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,5 +1,5 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Grid } from "@mui/material";
+import { Grid, Skeleton } from "@mui/material";
 import { createClient } from "contentful";
 import { useEffect, useState } from "react";
 import BlogCard from "../components/BlogCard";
@@ -9,42 +9,56 @@ import { contentfulConfig } from "../lib/Constants";
 const Blog = () => {
 	const [blogs, setBlogs] = useState<any>();
 	const [open, setOpen] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const client = createClient({
 		accessToken: contentfulConfig.CONTENTFUL_TOKEN as string,
 		space: contentfulConfig.CONTENTFUL_SPACE as string,
 	});
 
 	useEffect(() => {
+		setLoading(true);
 		client.getEntries().then((res) => {
+			setLoading(false);
 			setBlogs(res.items);
-			console.log(res.items);
 		});
 	}, []);
 
 	return (
 		<>
 			<Grid container spacing={4}>
-				{blogs &&
-					blogs.map((blog: any, index: any) => {
-						return (
-							<>
-								<Grid xs={6} md={3} xl={2} item key={index}>
-									<BlogCard
-										heading={blog.fields.heading}
-										description={blog.fields.description}
-										setOpen={setOpen}
+				{loading
+					? [...Array(20)].map((x, i) => {
+							return (
+								<Grid xs={6} md={3} xl={2} item key={i}>
+									<Skeleton
+										variant="rectangular"
+										animation="pulse"
+										height={150}
 									/>
 								</Grid>
-								<Modal
-									open={open}
-									setOpen={setOpen}
-									heading={blog.fields.heading}
-									modified={blog.sys.updatedAt}
-									body={documentToReactComponents(blog.fields.body)}
-								/>
-							</>
-						);
-					})}
+							);
+					  })
+					: blogs &&
+					  blogs.map((blog: any, index: any) => {
+							return (
+								<>
+									<Grid xs={6} md={3} xl={2} item key={index}>
+										<BlogCard
+											heading={blog.fields.heading}
+											description={blog.fields.description}
+											setOpen={setOpen}
+										/>
+									</Grid>
+									<Modal
+										open={open}
+										setOpen={setOpen}
+										heading={blog.fields.heading}
+										modified={blog.sys.updatedAt}
+										body={documentToReactComponents(blog.fields.body)}
+									/>
+								</>
+							);
+					  })}
 			</Grid>
 		</>
 	);
